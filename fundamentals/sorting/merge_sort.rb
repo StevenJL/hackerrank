6a35f13cb8b4c63dc7c336d4ee7ed60a0ede5592
Steven Li <stevenjli@gmail.com>
2018-02-06 20:32:18 -0800

    restructuring

--- fundamentals/sorting/merge_sort.rb
+++ fundamentals/sorting/merge_sort.rb
@@ -1,20 +1,3 @@
-# Selection Sort
-
-def selection_sort(list)
-  length = list.length
-  for i in (0..(length - 3))
-    for j in ((i+1)..(length - 1))
-      if list[i] > list[j]
-        list[i], list[j] = list[j], list[i]
-      end
-    end
-  end
-  list
-end
-
-x = [41,13,45,3,1,2,3,10]
-selection_sort(x)
-
 # Merge sort
 
 def merge_sort(list)
